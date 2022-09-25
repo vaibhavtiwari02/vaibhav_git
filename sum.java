478ffb333f4b32c49397d0bc6577151c89d2812d
vaibhav <18vaibhav002@gmail.com>
2022-09-25 13:48:09 +0530

    maine multiply kiya

--- sum.java
+++ sum.java
@@ -1,6 +1,6 @@
 class sum{
     public static void main(String[] args) {
         int a=10, b=20;
-        System.out.println("the sum is:"+ (a+b));
+        System.out.println("the product is:"+ (a*b));
     }
 }
